403bdd153a55d4fce470c6caa408ef60adad49c2
sai-Mercury <luixiao1223@sina.com>
2020-06-12 17:53:50 +0800

    edit

--- LeNet-5/src/train.py
+++ LeNet-5/src/train.py
@@ -1,4 +1,8 @@
 import torch
 import torchvision.datasets as datasets
 
-mnist_trainset = datasets.MNIST(root='../MNIST', train=True, download=True, transform=None)
+mnist_trainset = datasets.MNIST(root='./', train=True, download=True, transform=None)
+mnist_testset = datasets.MNIST(root='./', train=False, download=True, transform=None)
+
+for item in mnist_trainset:
+    img, num = item[0], item[1]
