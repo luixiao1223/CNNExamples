bbd653a29913542e4e1db0c22f95c5d990fc43cb
sai-Mercury <luixiao1223@sina.com>
2020-06-15 19:44:55 +0800

    edit

--- LeNet-5/src/model/LeNet5.py
+++ LeNet-5/src/model/LeNet5.py
@@ -5,7 +5,22 @@ import torch.nn.functional as functional
 class LeNet5(nn.Module):
     def __init__(self):
         super(LeNet5, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, (3, 3), padding=1)
-        
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2, bias=True)
+        self.max_pool_1 = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0, bias=True)
+        self.max_pool_2 = nn.MaxPool2d(kernel_size=2)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
     def forward(self, x):
-        pass
+        x = nn.functional.relu(self.conv1(x))
+        x = self.max_pool_1(x)
+        x = nn.functional.relu(self.conv2(x))
+        x = self.max_pool_2(x)
+        x = x.view(-1, 16*5*5)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
